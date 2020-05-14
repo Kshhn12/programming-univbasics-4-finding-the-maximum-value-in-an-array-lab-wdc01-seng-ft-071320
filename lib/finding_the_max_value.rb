e1b15a6aacbb6d96a4590ad332ca0a7a1a2f4a4f
Kevin Sheehan <kshhn12@gmail.com>
2020-05-14 14:21:59 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   max = array[0]
-  for(count = 0; count < array.length; count++)
-    if(array[count] > max)
+  for count = 0; count < array.length; count++
+    if array[count] > max
       max = array[count]
     end
   return max
